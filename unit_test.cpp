6d8cc922536d7d6d1e51390bc948e16c2add339e
Justin Gafford <jgaff001@ucr.edu>
2019-04-26 13:00:04 -0700

    Added Individual class test cases in unit_test.cpp

--- unit_test.cpp
+++ unit_test.cpp
@@ -16,6 +16,59 @@ class OpMock: public Base {
         virtual string stringify() { return "7.5"; }
 };
 
+TEST(IndvClassTest, OpTest)  {
+
+Op* op1 = new Op(5.0); 
+Op* op2 = new Op(2.0); 
+
+EXPECT_EQ(op1 -> evaluate(), 5.0);
+EXPECT_EQ(op2 ->stringify(), "2.000000");
+}
+TEST(IndvClassTest, AddTest)  {
+Op* op1 = new Op(5.0); 
+Op* op2 = new Op(2.0); 
+
+Add* add1 = new Add(op1, op2);
+EXPECT_EQ(add1->evaluate(), 7);
+EXPECT_EQ(add1->stringify(), "5.000000+2.000000");
+}
+
+TEST(IndvClassTest, SubTest)  {
+Op* op1 = new Op(5.0); 
+Op* op2 = new Op(2.0); 
+
+Sub* sub1 = new Sub(op1 , op2);
+EXPECT_EQ(sub1->evaluate(), 3);
+EXPECT_EQ(sub1->stringify(), "5.000000-2.000000");
+}
+
+TEST(IndvClassTest, MultTest)  {
+Op* op1 = new Op(5.0); 
+Op* op2 = new Op(2.0); 
+
+Mult* mult1 = new Mult(op1 , op2);
+EXPECT_EQ(mult1  ->evaluate(),  10);
+EXPECT_EQ(mult1  ->stringify(), "5.000000*2.000000");
+}
+
+TEST(IndvClassTest, DivTest)  {
+Op* op1 = new Op(5.0); 
+Op* op2 = new Op(2.0); 
+
+Div* div1 = new Div(op1  , op2);
+EXPECT_EQ(div1  ->evaluate(),  2.5);
+EXPECT_EQ(div1  ->stringify(), "5.000000/2.000000");
+}
+
+TEST(IndvClassTest, PowTest)  {
+Op* op1 = new Op(5.0); 
+Op* op2 = new Op(2.0); 
+
+Pow* pow1 = new Pow(op1, op2);
+EXPECT_EQ(pow1  ->evaluate(),  25);
+EXPECT_EQ(pow1  ->stringify(), "5.000000**2.000000");
+}
+
 TEST(ArithTest, OpNumber)  {
 
 Op* op1 = new Op(5.0); 
